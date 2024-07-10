2cd3f74874c628b33c62ade7f682ff7ca20e47f4
Kursant <wanzkihn@gmail.com>
2024-07-10 18:54:25 +0300

    asd

---
+++ rgb-web-studio-frontend/pages/create-user.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+	create user
+	<a href="/">asdasdsad</a>
+</div>
+</template>
+
+<script>
+export default {
+	name: "create-user"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ rgb-web-studio-frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="bg-redd">index</div>
+	<a href="/create-user">fvvfdfv</a>
+</template>
+
+<script>
+export default {
+	name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
